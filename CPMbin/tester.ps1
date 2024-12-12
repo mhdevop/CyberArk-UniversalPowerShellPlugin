11cf759817ac8f50658bc2507e0de7a3a34f3ff8
Micah Holt <miner-stale.7k@icloud.com>
2024-12-11 17:14:09 -0700

    Updated license and project structure updates

---
+++ CPMbin/tester.ps1
@@ -0,0 +1,180 @@
+<#
+.SYNOPSIS
+    Abstracts and lowers the coding threshold to interact with CyberArk integrations to the PowerShell language
+.DESCRIPTION
+    Abstracts the deep C# coding experience and voids the need for Prompts/Process files and boils the entire experience down to this PowerShell script.
+    The "magic" of this solution is that it transparently passes variables within the runspace that the C# plugin creates and NOT over the command line. Maybe 
+    you've noticed in your Windows Event viewer but PowerShell logs arguments passed so it's crazy to feed in a password as it shows up in plaintext. Here,
+    my plugin transparently attaches all the normal CyberArk variables you could need. In other words, work 99% in PowerShell, not in C# or Prompt/Process files.
+.EXAMPLE
+    On the CPM, run the following command one directory up from the "bin" folder to test the plugin (reference: https://docs.cyberark.com/pam-self-hosted/latest/en/content/pasimp/plug-in-netinvoker_test.htm?tocpath=Developer%7CCreate%20extensions%7CCreate%20CPM%20plugins%7CCredentials%20Management%20.NET%20SDK%7C_____5)
+    CANetPluginInvoker.exe user.ini changepass CyberArk.Extensions.Plugin.RealPowerShell.dll true
+.NOTES
+    Author: https://github.com/mhdevop/CyberArk-RealPowerShell
+    Date:   2024-12-06
+    Version: 1.0
+#>
+
+#intro
+Write-Output "Welcome to RealPowerShell Skeleton code! This plugin will show you very simple 'logic' examples and explain the variables you have access to"
+
+# prepare the "success" variable that will be read by our C# plugin code and reported back to CPM. By default we set it to failure and only set it to the magic string
+# if we are certain that it succeeded
+$global:RealPowerShellPassOrFail = "false" # in YOUR logic you need to set to =  "PowerShell Success"
+
+# the CyberArk "properties" (safe, username, etc.) are all stored in a single PowerShell Hashtable for a consolidated experience.
+# in this example, we get the value of "debug" and if it's true we print all the variables that RealPowerShell has passed to this script
+if ($CARKTargetHashtable["debug"]){Write-Output (Get-Variable * | where {$_.name -like "CARK*"} | Out-String )}
+
+<#  SAMPLE values that are available during runtime. "hashtable" contain account properties and "cred" objects contain the actual passwords
+
+Name                           Value                                                                                    
+----                           -----                                                                                    
+CARKLogonHashtable             {username, PolicyID, foldername, objectname...}                                          
+CARKLogonPSCredObject          System.Management.Automation.PSCredential                                                
+CARKReconHashtable             {username, PolicyID, foldername, objectname...}                                          
+CARKReconPSCredObject          System.Management.Automation.PSCredential                                                
+CARKTargetHashtable            {objectname, username, debug, foldername...}                                             
+CARKTargetPSCredObjectCurrent  System.Management.Automation.PSCredential                                                
+CARKTargetPSCredObjectNew      System.Management.Automation.PSCredential   
+#>
+
+# EXAMPLE 1 - the "username" is the value we want to get out of each hashtable based on type of account: target/logon/reconcile
+Write-Output "Target Username = $($CARKTargetHashtable["username"])"
+Write-Output "Logon Username = $($CARKLogonHashtable["username"])"
+Write-Output "Reconcile Username = $($CARKReconHashtable["username"])"
+
+# EXAMPLE 2 - the "username" is the value we want to get out of each hashtable based on type of account: target/logon/reconcile
+Write-Output "Target Username DEBUG = $($CARKTargetHashtable["debug"])"
+Write-Output "Logon Username DEBUG = $($CARKLogonHashtable["debug"])"
+Write-Output "Reconcile Username DEBUG = $($CARKReconHashtable["debug"])"
+
+# EXAMPLE 3 - pass the encrypted and packaged user/password directly to a command as a "Credential" (commented out so you don't hit AD with fake/testing accounts)
+# Get-ADUser AcountNameHere -Credential $CARKTargetPSCredObjectCurrent
+
+# EXAMPLE 4 - Don't use these example unless it's a last resort! If you need the "plaintext" password you can get it but you should strive for example #3 above as it plays
+#               nicely with so many PowerShell commandlets by default
+Write-Output "Target Username PW = First character of password $($CARKTargetPSCredObjectCurrent.getnetworkcredential().password[0])******"
+Write-Output "Logon Username  PW = First character of password $($CARKLogonPSCredObject.getnetworkcredential().password[0])******"
+Write-Output "Reconcile Username  PW = First character of password $($CARKReconPSCredObject.getnetworkcredential().password[0])******"
+
+
+function Invoke-Verify{
+
+    Write-Output "Running Verify Now (really we're just testing if we can login)"
+
+    # CHANGEME - add all your logic here. Otherwise just some fake testing logic here
+    Write-Output "Logging in to XYZ application using the current Target Credentials with account $($CARKTargetPSCredObjectCurrent.username)"
+    Write-Output "...Here's some REST API code that YOU supply to take in the credentials..."
+    Write-Output "...More of YOUR code that checks if logging in worked our not..."
+    Write-Output "...Let's assume your logic says the credentials worked..."
+
+    return $true
+}
+
+
+function Invoke-ChangePass{
+
+    Write-Output "Running Changepass Now"
+
+    # CHANGEME - add all your logic here. Otherwise just some fake testing logic here
+    Write-Output "Logging in to XYZ application using the current Target Credentials with account $($CARKTargetPSCredObjectCurrent.username)"
+    Write-Output "...Here's some REST API code that YOU supply to take in the credentials..."
+
+    Write-Output "Passing in the Current Credentials to the application:"
+    $CARKTargetPSCredObjectCurrent
+
+    Write-Output "Now the app is asking for the new password to set so we better use the one CPM is telling us to use or else we'll be mismatched if we try and generate our own"
+    $CARKTargetPSCredObjectNew
+
+    Write-Output "...More of YOUR code that does the password change..."
+    Write-Output "...Let's assume your logic says the credentials worked..."
+
+    return $true
+}
+
+
+function Invoke-ReconcilePass{
+
+    Write-Output "Running Reconcile Now"
+
+    # CHANGEME - add all your logic here. Otherwise just some fake testing logic here
+    Write-Output "Logging in to XYZ application using the current Reconcile Credentials with account $($CARKReconPSCredObject.username)"
+    Write-Output "...Here's some REST API code that YOU supply to take in the credentials..."
+
+    Write-Output "Passing in the Current RECONCILE Credentials to the application:"
+    $CARKReconPSCredObject
+
+    Write-Output "Now the app is asking for the new password to the TARGET account to set"
+    $CARKTargetPSCredObjectNew
+
+    Write-Output "...More of YOUR code that does the password RECONCILE change..."
+    Write-Output "...Let's assume your logic says the credentials worked..."
+
+    return $true
+}
+
+
+
+
+
+##### MAIN Logic ##############################
+
+# CPM will only do one "operation" at time: changepass, verifypass, preconcile, reconcile. If you remember in the C# code, we "ignore" logic for login/prereconile/verify
+# so that all logic is handled in this script. In other words, this is the part CyberArk won't be thrilled at in that we're abstracting out their SDK and boiling it down
+# which causes an intential "mismatch" between the phase the logic actually occurs in. If this is confusing then just remember this plugin comes with no support.
+Write-Output "CPM Operation Attempt = $CAOperation"
+if ($CAOperation -eq "changepass")
+{
+
+    Write-Output "Starting CUSTOM verify function you coded"
+    # to change a password we should verify our current passwords works to login first
+    if (Invoke-Verify)
+    {
+
+        Write-Output "Starting CUSTOM changepass function you coded since we passed the verify (logon) portion so we know the password works to change"
+        # now do the password change
+        if (Invoke-ChangePass)
+        {
+            Write-Output "Congrats, you logged in + changed the password so we know this dummy plugin worked!"
+            # if we changed the password then we have success! Set the variable for it to be written out at the end of this script
+            $global:RealPowerShellPassOrFail = "PowerShell Success"
+        }
+        else{
+            Write-Error "Failed to change the password"
+        }
+    }
+}
+elseif ($CAOperation -eq "verify")
+{
+    Write-Output "Starting CUSTOM verify function you coded"
+    # to verify a password all we do is logon essentially
+    if (Invoke-Verify)
+    {
+        Write-Output "Congrats, all you wanted to do this was time was verify (logon) and your code confirmed it"
+        # if we changed the password then we have success! Set the variable for it to be written out at the end of this script
+        $global:RealPowerShellPassOrFail = "PowerShell Success"
+    }
+    else{
+        Write-Error "Failed to verify the passwords"
+    }
+}
+elseif ($CAOperation -eq "reconcile")
+{
+
+    Write-Output "Starting CUSTOM reconcile function you coded"
+    # now do the password change
+    if (Invoke-ReconcilePass)
+    {
+        Write-Output "Congrats your custom code that uses reconcile credentials to set the target credentials worked somehow"
+        # if we reconciled the password then we have success! Set the variable for it to be written out at the end of this script
+        $global:RealPowerShellPassOrFail = "PowerShell Success"
+    }
+    else{
+        Write-Error "Failed to reconcile the password"
+    }
+}
+
+# for the grand finale, write the pass/fail results of the entire plugin so that CPM can read it. As a reminder, anything but the magic phrase "PowerShell Success"
+# means that the plugin failed
+write-output $global:RealPowerShellPassOrFail
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# CA-RealPowerShell
+# CyberArk Real PowerShell
+## _A plugin to make creating plugins easier_
+
+CARP (CyberArk Real PowerShell) is a prepopulated template of the   [CyberArk .NET SDK](https://docs.cyberark.com/pam-self-hosted/latest/en/content/pasimp/plug-in-netinvoker.htm?tocpath=Developer%7CCreate%20extensions%7CCreate%20CPM%20plugins%7CCredentials%20Management%20.NET%20SDK%7C_____0) that abstracts "hard" code like C# and Prompts/Process files down to simple PowerShell code! In other words, **you can write 99% (or more) of your code in PowerShell without ever needing to touch or know those other painful languages.** Here are the highlighted features:
+
+- Logic and error handling to intelligently decide the context the plugin itself is being used in (eg [Target](https://docs.cyberark.com/pam-self-hosted/latest/en/content/landing%20pages/lpbuiltinplugins.htm) vs. [Usage "Service"](https://docs.cyberark.com/pam-self-hosted/latest/en/content/landing%20pages/lpserviceplugins.htm)). This means you don't have to decide what kind of plugin you need, you can use the **same** plugin on both! (If you know anything about plugin development which I assume you do being here then you know the value of this)
+- Limited to no C# coding needed (unless you have advanced needs to expand on this code). Unless you know c# well, most people want to pay a vendor to write this level of code for them when in reality, a simple PowerShell script would do just fine
+- Sample PowerShell script to show just how easy it is to use this plugin for nearly all your needs
+- The best part - **no** Prompts/Process ini files to deal with (sorry if you love those but I find them painful to explain and edit)
+- ✨Magic ✨
+
+
+## Disclaimers (who doesn't like a list of those)
+
+These days you can't be too careful so "for the record" note the following:
+
+- This code has ++**not**++ been endorsed, supported, reviewed, or encouraged by CyberArk or their affiliates
+- While I have tested this code myself to the nth degree, YOU are solely responsible for it being used for your purposes. I take no repsonsibility for any and all issues caused by its usage.
+- I cannot and will not provide "support" for this plugin. Anyone in the CyberArk business knows that consultants at minimum make ~$250+ per hour so as much as I'd love to support it, it's unfair, let alone unrealistic for me to do so
+- I built this code during my own personal time (not work hours), using my own personal PC (not work), and from my own brain 
+- Regarding the code:
+  - My code does not follow some best practices to which I admit directly and openly. My objective was not to win first prize on aeasthetics.
+  - My primary object was to make code that "works" and is "easier to unerstand" rather than "efficiency" or "minimal resources". (eg there is plenty of redundency and room for fancy things like abstract classes, sharing methods, etc. but I chose not to use those to try and keep the code linear and easier to understand for the masses)
+  - If you don't like it, think it's trash, hate it, or etc. by all means I encourage YOU to change it on your end. I can't and never will think of everything under the sun
